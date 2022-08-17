b5c518123d72cbba3f845e03e037c3e6fc12a71e
Joanne <jl7105@nyu.edu>
2022-08-17 11:28:40 -0400

    protect routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,11 +29,34 @@ function App() {
         <div className='content'>
           {/* For react-router-dom v6, <Switch>/<Routes> tag looks through its children <Route>s and renders the first one that matches the current URL. */}
           <Routes>
+
             {/* Components and their URLs here: */}
             <Route exact path="/" element={<Home />} />
-            <Route path="/register" element={<PublicRoute><Register /></PublicRoute>} />
-            <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
-            <Route path="/premium-content" element={<PrivateRoute><PremiumContent /></PrivateRoute>} />
+
+            {/* Redirect route */}
+            <Route path="/register" element={
+                <PublicRoute>
+                  <Register />
+                </PublicRoute>
+              } 
+            />
+
+            {/* Redirect route */}
+            <Route path="/login" element={
+                <PublicRoute>
+                  <Login />
+                </PublicRoute>
+              } 
+            />
+
+            {/* Protect route */}
+            <Route path="/premium-content" element={
+                <PrivateRoute>
+                  <PremiumContent />
+                </PrivateRoute>
+              } 
+            />
+
           </Routes>
         </div>
       </BrowserRouter>
