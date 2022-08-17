49344f0002e7889f9d96ec990139b77f9015b46c
Joanne <jl7105@nyu.edu>
2022-08-16 23:34:41 -0400

    premiumContent component

--- frontend/src/PremiumContent.js
+++ frontend/src/PremiumContent.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { getUser, resetUserSession} from './service/AuthService'
 
-function PremiumContent() {
+function PremiumContent(props) {
+  const user = getUser();
+  const name = user !== 'undefined' && user ? user.name : '';
+
+  const logoutHandler = () => {
+    resetUserSession();
+    props.history.push('/login')
+  }
   return (
-    <div>This is the Premium Content Page!</div>
+    <div>
+      This is the Premium Content Page!
+      <br/>
+      Hello {name}! You have been logged in!
+      <br/>
+      <input type="button" value="Logout" onClick={logoutHandler} />
+    </div>
   )
 }
 
