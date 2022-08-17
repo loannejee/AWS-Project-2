1efdb4fe5c2bb6664db9d4f505cb08c301226942
Joanne <jl7105@nyu.edu>
2022-08-17 13:43:19 -0400

    verify url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,8 +12,50 @@ import Login from './Login';
 import PremiumContent from './PremiumContent';
 import PublicRoute from "./routes/PublicRoute";
 import PrivateRoute from "./routes/PrivateRoute";
+import env from "react-dotenv";
+import React, { useEffect, useState } from 'react';
+import { getUser, getToken, setUserSession, resetUserSession } from "./service/AuthService";
+import axios from "axios";
+
+const verifyTokenAPIURL = `${env.VERIFY_URL}`;
+const X_API_KEY = `${env.X_API_KEY}`;
 
 function App() {
+  const [isAuthenticating, setAuthenticating] = useState(true);
+
+  // As soon as component mounts
+  useEffect(() => {
+    const token = getToken();
+    if (token === 'undefined' || token === undefined || token === null || !token) {
+      return;
+    }
+
+    const requestConfig = {
+      headers: {
+        'x-api-key': X_API_KEY
+      }
+    }
+
+    const requestBody = {
+      user: getUser(),
+      token: token
+    }
+
+    axios.post(verifyTokenAPIURL, requestBody, requestConfig).then((response) => {
+      setUserSession(response.data.user, response.data.token);
+      setAuthenticating(false);
+    }).catch(() => {
+      resetUserSession();
+      setAuthenticating(false);
+    })
+
+  }, [])
+
+  const token = getToken();
+  if (isAuthenticating && token) {
+    return <div className="content">Authenticating...</div>
+  }
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -35,26 +77,26 @@ function App() {
 
             {/* Redirect route */}
             <Route path="/register" element={
-                <PublicRoute>
-                  <Register />
-                </PublicRoute>
-              } 
+              <PublicRoute>
+                <Register />
+              </PublicRoute>
+            }
             />
 
             {/* Redirect route */}
             <Route path="/login" element={
-                <PublicRoute>
-                  <Login />
-                </PublicRoute>
-              } 
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
             />
 
             {/* Protect route */}
             <Route path="/premium-content" element={
-                <PrivateRoute>
-                  <PremiumContent />
-                </PrivateRoute>
-              } 
+              <PrivateRoute>
+                <PremiumContent />
+              </PrivateRoute>
+            }
             />
 
           </Routes>
